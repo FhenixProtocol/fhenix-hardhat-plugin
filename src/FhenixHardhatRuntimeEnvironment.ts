57229fa586f471eead233f5bdb51b5372f22d3b3
Assaf Morami <assaf.morami@gmail.com>
2024-01-25 14:06:43 +0200

    More robust docker management code

--- src/FhenixHardhatRuntimeEnvironment.ts
+++ src/FhenixHardhatRuntimeEnvironment.ts
@@ -1,18 +1,28 @@
 import axios from "axios";
-import { WebSocketProvider } from "ethers";
+import child_process from "child_process";
+import { JsonRpcProvider } from "ethers";
 import { FhenixClient } from "fhenixjs";
 
-type FhenixHardhatRuntimeEnvironmentConfig = {
+import { config as packageConfig } from "../package.json";
+
+interface FhenixHardhatRuntimeEnvironmentConfig {
   /// rpcPort defaults to 8545
   rpcPort?: number;
   /// wsPort defaults to 8548
   wsPort?: number;
   /// faucetPort defaults to 3000
   faucetPort?: number;
-};
+}
+
+interface Container {
+  name: string;
+  ports: string;
+  image: string;
+}
 
 export class FhenixHardhatRuntimeEnvironment {
   public readonly fhenixjs: FhenixClient;
+  public readonly ready: Promise<void>;
 
   public constructor(
     public config: FhenixHardhatRuntimeEnvironmentConfig = {
@@ -26,7 +36,75 @@ export class FhenixHardhatRuntimeEnvironment {
     this.config.faucetPort = this.config.faucetPort ?? 3000;
 
     this.fhenixjs = new FhenixClient({
-      provider: new WebSocketProvider(`ws://localhost:${this.config.wsPort}`),
+      provider: new JsonRpcProvider(`http://localhost:${this.config.rpcPort}`),
+    });
+
+    this.ready = new Promise((resolve, reject) => {
+      let stdout: string;
+
+      try {
+        stdout = child_process
+          .execSync(
+            `docker ps -a --format '{"name": "{{ .Names }}", "ports": "{{ .Ports }}", "image": "{{ .Image }}"}'`,
+          )
+          .toString();
+      } catch (error) {
+        return reject(error);
+      }
+
+      const [existingLocalfhenix]: Container[] = stdout
+        .split("\n")
+        .map((line) => JSON.parse(line))
+        .filter((container: Container) => container.name === "localfhenix");
+
+      const run = () => {
+        try {
+          child_process.execSync(`docker rm -f localfhenix`);
+          child_process.execSync(
+            `docker run -d --rm -p "${this.config.rpcPort}":8547 -p "${this.config.wsPort}":8548 -p "${this.config.faucetPort}":3000 --name localfhenix "${packageConfig.image}"`,
+          );
+        } catch (error) {
+          return reject(error);
+        }
+      };
+
+      if (!existingLocalfhenix) {
+        run();
+        return resolve();
+      }
+
+      if (existingLocalfhenix.image !== packageConfig.image) {
+        run();
+        return resolve();
+      }
+
+      const portMappings = Array.from(
+        existingLocalfhenix.ports.matchAll(/0\.0\.0\.0:\d+->\d+\/tcp/g),
+      ).map((match) => match[0]);
+
+      if (portMappings.length !== 3) {
+        run();
+        return resolve();
+      }
+
+      if (
+        !portMappings.includes(`0.0.0.0:${this.config.faucetPort}->3000/tcp`)
+      ) {
+        run();
+        return resolve();
+      }
+
+      if (!portMappings.includes(`0.0.0.0:${this.config.rpcPort}->8547/tcp`)) {
+        run();
+        return resolve();
+      }
+
+      if (!portMappings.includes(`0.0.0.0:${this.config.wsPort}->8548/tcp`)) {
+        run();
+        return resolve();
+      }
+
+      resolve();
     });
   }
 
