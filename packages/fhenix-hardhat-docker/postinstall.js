39420890f09d6325cfd11f369d0d100dacc2744a
Assaf Morami <assaf.morami@gmail.com>
2024-01-31 14:33:03 +0200

    Fix config import in postinstall.js

--- packages/fhenix-hardhat-docker/postinstall.js
+++ packages/fhenix-hardhat-docker/postinstall.js
@@ -1,6 +1,5 @@
 const child_process = require('child_process');
-
-const config = require('./src/config.json');
+const config = require(require('path').join(__dirname, "src", "config.json"));
 
 console.log('Removing old localfhenix instances...');
 child_process.execSync(`docker rm -f "${config.container_name}" 2> /dev/null`)
