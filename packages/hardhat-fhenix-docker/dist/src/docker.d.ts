7f5cb1842bf72d9938820825263451aa1fe3ce76
Assaf Morami <assaf.morami@gmail.com>
2024-01-31 12:42:54 +0200

    Rename packages directories to match packages name

--- packages/hardhat-fhenix-docker/dist/src/docker.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const isRunningContainer: (name: string) => boolean;
-export declare const runLocalFhenixSeparateProcess: (port: number, ws: number, faucet: number, image: string) => Promise<void>;
-export declare const stopLocalFhenix: () => void;
-//# sourceMappingURL=docker.d.ts.map
